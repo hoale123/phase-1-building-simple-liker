5cc3603de6f676646c4eaf46f0449eb1d8c0c52e
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-09 20:41:10 -0500

    commit tree ab8a0fa285d4ca0e33dbb210950d99c44ddbb705

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// get all necessary element first
+// make the heart filled , get the element heart
+const alertHeartLiker = document.querySelectorAll('.like-glyph')
 
+function serverCallback(e) {
+  const heart = e.target
+  mimicServerCall()
+    .then(() => {
+      if( heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else{
+        heart.innerText = EMPTY_HEART;
+        heart.className = ""
+      }
+    })
+    .catch((err) => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = err;
+      setTimeout(() => modal.className = "hidden", 3000)
+    });
+  }
 
-
+  for (const glyph of alertHeartLiker) {
+    glyph.addEventListener("click", serverCallback)
+  }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
